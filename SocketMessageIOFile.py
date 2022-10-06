0fc620aa9b6499cc8eb91e3eb198bd6a427e6866
Harlan Howe <hhowe7@gatech.edu>
2022-10-06 12:01:24 -0500

    Documented receive_message_from_socket() to explain better. Updated receive_message_from_socket() so that it only requests as much data as we want to receive.

--- SocketMessageIOFile.py
+++ SocketMessageIOFile.py
@@ -1,6 +1,10 @@
 import socket, struct, typing
 
 class SocketMessageIO:
+    """
+    A utility class that makes it easy to send and receive messages from a socket in the format of a packed length of
+    the message, followed by the message.
+    """
     def receive_message_from_socket(self, connection: socket) -> str:
         """
         Waits until the socket provides a message in the form of a packed length of the message and the message itself.
@@ -17,8 +21,14 @@ class SocketMessageIO:
         message_length = struct.unpack('>I', message_length_str)[0]
         received_length = 0
         message = ""
+        # we're going to ask to receive data from the socket, but it may arrive in separate sections of data. For
+        # instance, we might be expecting a message of length 512, but it could come in two "chunks" of 200 and 312.
+        # Moreover, we're only asking for data from the socket up to size 1024, so a longer message might require
+        # several reads of the socket.
+        # But the goal is to get the entire message.
         while received_length < message_length:
-            chunk_o_data = connection.recv(1024).decode()
+            request_length = min(1024, message_length-received_length)
+            chunk_o_data = connection.recv(request_length).decode()
             message += chunk_o_data
             received_length += len(chunk_o_data)
         return message
